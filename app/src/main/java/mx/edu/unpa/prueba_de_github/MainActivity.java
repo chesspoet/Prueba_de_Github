3ed7fe3e8dd2355cb49e67b258cb55db3a1af24b
chesspoet <oswrher@gmail.com>
2021-05-21 13:20:46 -0500

    Initial commit

---
+++ app/src/main/java/mx/edu/unpa/prueba_de_github/MainActivity.java
@@ -0,0 +1,15 @@
+package mx.edu.unpa.prueba_de_github;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
