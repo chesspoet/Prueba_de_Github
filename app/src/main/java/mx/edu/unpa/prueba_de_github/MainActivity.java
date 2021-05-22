0421cde9982377998b0aa1735b199d05e4244744
chesspoet <oswrher@gmail.com>
2021-05-22 01:38:31 -0500

    adsadas

--- app/src/main/java/mx/edu/unpa/prueba_de_github/MainActivity.java
+++ app/src/main/java/mx/edu/unpa/prueba_de_github/MainActivity.java
@@ -3,6 +3,7 @@ package mx.edu.unpa.prueba_de_github;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
 
     }
 }
